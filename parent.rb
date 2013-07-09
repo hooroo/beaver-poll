ef63634252563190a758cd261a29e11f6369c255
James Dunwoody <jamesd@hooroo.com>
2013-07-09 16:05:06 +1000

    initial commit

---
+++ parent.rb
@@ -0,0 +1,11 @@
+#daemon
+#exec('ruby poller.rb')
+#puts('finished')
+
+p1 = fork { sleep 0.1 }
+p2 = fork { sleep 0.2 }
+Process.detach(p1)
+Process.waitpid(p2)
+sleep 2
+system("ps -ho pid,state -p #{p1}")
+
